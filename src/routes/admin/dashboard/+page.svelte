bba5fe3f82075d23eaa3cecc240f7f3d08cf41fd
vishal-sujosu <vishal.jadhav@sujosu.com>
2025-09-10 17:18:56 +0530

    Refactor routes names

--- src/routes/admin/dashboard/+page.svelte
+++ src/routes/admin/dashboard/+page.svelte
@@ -7,9 +7,9 @@
 <div>
     admin dashboard
 
-    <a href="/contractors/2/dashboard">Go to Contractor </a>
+    <a href="/tenant/2/dashboard">Go to Contractor </a>
 
-    <a href="/sub-contractors/3/dashboard">Go to sub-Contractor </a>
+    <a href="/member/3/dashboard">Go to sub-Contractor </a>
 </div>
 
 <Content title="Home"/>
