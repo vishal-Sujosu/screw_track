a77e012d74231e637b479bbcca564f94951b775b
vishal-sujosu <vishal.jadhav@sujosu.com>
2025-09-12 17:22:02 +0530

    adding sve;te-charts

--- src/routes/tenant/[id]/dashboard/+page.svelte
+++ src/routes/tenant/[id]/dashboard/+page.svelte
@@ -1,3 +1,37 @@
+<script >
+	import '@carbon/charts-svelte/styles.css'
+      import { onMount } from 'svelte';
+  let ChartComponent;
+
+  onMount(async () => {
+    const module = await import('@carbon/charts-svelte');
+    ChartComponent = module.BarChartSimple;  // or any chart you need
+  });
+</script>
 <div>
     Contractor dashboard
 </div>
+
+{#if ChartComponent}
+  <svelte:component this={ChartComponent} data={[
+		{ group: 'Qty', value: 65000 },
+		{ group: 'More', value: 29123 },
+		{ group: 'Sold', value: 35213 },
+		{ group: 'Restocking', value: 51213 },
+		{ group: 'Misc', value: 16932 }
+	]} options={{
+		theme: 'white',
+		title: 'Simple bar (discrete)',
+		height: '400px',
+        fileDownload: {
+            filename: 'dummy-filename'
+        },
+        
+		axes: {
+			left: { mapsTo: 'value' },
+			bottom: { mapsTo: 'group', scaleType: 'labels' }
+		},
+        
+	}}  />
+{/if}
+
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	ssr: {
+		noExternal: process.env.NODE_ENV === 'production' ? ['@carbon/charts'] : []
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
